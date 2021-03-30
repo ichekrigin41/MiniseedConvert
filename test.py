604d302894fadc6f515b73bb0c2ff896a8b3cd39
ichekrigin41 <ichekrigin@gmail.com>
2021-03-31 11:23:02 +1200

    Changing DIRS Function

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 from obspy import read
 from numpy import savetxt
 import os
+from datetime import datetime
+import shutil
 
 
+today=datetime.now()
+if today.hour  <12:
+    h="00"
+else:
+    h="12"
+
+
+
+os.mkdir("/home/zoohan/Desktop/cnvrt/"+"Converted_"+today.strftime('%Y%m%d')+h)
+
+#print(type(CONVERTED_DIR))
+#os.chdir(os.mkdir("/home/zoohan/Desktop/cnvrt/"+"Converted_"+today.strftime('%Y%m%d')+h))
 #Convert .miniseed to txt format 
 
 localdr=os.listdir("21")
 
+#Converted_2021033100
+
+
 def DataConvert(fileName,data,data2,data3,data4):
+    os.chdir("/home/zoohan/Desktop/cnvrt/"+"Converted_"+today.strftime('%Y%m%d')+h)
     fileOut=fileName
     fileOut=open(fileName,'w')
-
+    
     for line in data:
         fileOut.write(str(line))
         fileOut.write('\n')
@@ -31,17 +49,32 @@ def DataConvert(fileName,data,data2,data3,data4):
     fileOut.write('\n')
 
 
-
-for lines in localdr:
+def dataProcess(workingPath):
+    #for lines in localdr:
     print (lines)
-    miniseedData=read("/home/zoohan/Desktop/test/21/"+lines)
+    #miniseedData=read("/home/zoohan/Desktop/cnvrt/21/"+lines)
+    miniseedData=read(workingPath+'/'+lines)
     channels = miniseedData 
     ch_0=miniseedData[0].data
     ch_1=miniseedData[1].data
     ch_2=miniseedData[2].data
     ch_3=miniseedData[3].data
-
     DataConvert('CONVERTED_'+lines[24:38],ch_0,ch_1,ch_2,ch_3)
+
+print("Type Working Dir")
+path=str(input())
+
+for lines in localdr:
+    dataProcess(path)
+
+
+
+
+
+    
+
+
+
 
 
 
