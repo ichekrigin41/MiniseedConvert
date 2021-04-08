a60064c88f8d3c053cbf1df39513bab331ee3f8f
ichekrigin41 <ichekrigin@gmail.com>
2021-04-08 12:36:02 +1200

    Mover_Working

--- Getter2/getter_2/mover.py
+++ Getter2/getter_2/mover.py
@@ -1,5 +1,7 @@
 import os
-import re
+import shutil
+
+
 
 listNames=["_202101_","_202102_","_202103_","_202104_","_202105_","_202106_",
 "_202107_","_202108_","_202109_","_202110","_202111_","_202112_"]
@@ -14,42 +16,44 @@ f=[f'{str(index):0>2.2}' for index in range(1,13)]
 
 #mover2(testName2,f)
 
+
+
 def mover(fileName):
     if "_202101" in fileName:
-        #print("_202101")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_01')
         print(fileName)
     if "_202102" in fileName:
-        #print("_202102")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_02')
         print(fileName)
     if "_202103" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_03')
         print(fileName)
     if "_202104" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_04')
         print(fileName)
     if "_202105" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_05')
         print(fileName)
     if "_202106" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_06')
         print(fileName)
     if "_202107" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_07')
         print(fileName)
     if "_202108" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_08')
         print(fileName)
     if "_202109" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_09')
         print(fileName)
     if "_202110" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_10')
         print(fileName)
     if "_202111" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_11')
         print(fileName)
     if "_202112" in fileName:
-       # print("_202108")
+        shutil.move('/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/'+fileName,'/home/zoohan/Desktop/cnvrt/Getter2/getter_2/Downloaded/2021_12')
         print(fileName)
     else: return 0
 
@@ -60,3 +64,8 @@ for line in arr:
     mover(line)
 
 
+#mover3(arr,listNames)
+'''
+for line in arr:
+    mover3(line,listNames)
+'''
