ee99924c7df8159cfd5f3d1d815966eed36a397c
ichekrigin41 <ichekrigin@gmail.com>
2021-05-24 16:20:21 +1200

    dbfilter

--- Miniseed_Converter.py
+++ Miniseed_Converter.py
@@ -10,8 +10,13 @@ from pathlib import *
 import pymongo
 from pymongo import MongoClient
 import pathlib
+from pymongo import database
+from pymongo import cursor
 from sshtunnel import SSHTunnelForwarder
 import pprint
+import json
+import re
+
 
 MONGO_HOST = "192.168.1.34"
 MONGO_DB = "Archive"
@@ -27,10 +32,11 @@ server = SSHTunnelForwarder(
 
 server.start()
 
-client = pymongo.MongoClient('127.0.0.1', server.local_bind_port) # server.local_bind_port is assigned local port
+# server.local_bind_port is assigned local port
+client = pymongo.MongoClient('127.0.0.1', server.local_bind_port)
 db = client[MONGO_DB]
 DataDB = db["Data"]
-AddedFilesDB=db["AddedFiles"]
+AddedFilesDB = db["AddedFiles"]
 pprint.pprint(db.collection_names())
 
 
@@ -41,80 +47,148 @@ DataDB = mydb["Data"]
 '''
 
 
-
-
-
 print(db.list_collection_names())
 
-today=datetime.now()
-if today.hour  <12:
-    h="00"
+today = datetime.now()
+if today.hour < 12:
+    h = "00"
 else:
-    h="12"
+    h = "12"
 
-WorkDir=Path.cwd()
+WorkDir = Path.cwd()
 dir1 = f"{str(WorkDir)}/getter_2/Downloaded/Converted_{today.strftime('%Y%m%d')}{h}"
 if not os.path.isdir(dir1):
     os.mkdir(dir1)
 
 
+p = '/home/zoohan/Рабочий стол/convert_mongo/MiniseedConvert/tst/'
 
-#p = '/home/zoohan/Рабочий стол/convert_mongo/MiniseedConvert/tst/'
-
-#Ввод директории ОТКУДА конвертить
+# Ввод директории ОТКУДА конвертить
 print("Ввод директории ОТКУДА вести запись в БД")
-p=str(input())
-
-
+# p=str(input())
 
 
 localdr = os.listdir(p)
-elems=[]
+elems = []
 Files = []
 
 for elems in localdr:
-    elems=p+elems
-    Files+= [elems]
+    elems = p+elems
+    Files += [elems]
 
 print(Files)
 
 
-
-
 def DB_INSERT(el):
-    n=0
-    b=(read(el))
-    print (b)
-    
-    while n!=len(b.traces):
+    n = 0
+    b = (read(el))
+    print(b)
+
+    while n != len(b.traces):
         post = {
-        "network":b[n].stats.network,
-        "station":b[n].stats.station,
-        "location":b[n].stats.location,
-        "channel":b[n].stats.channel,
-        "starttime":datetime.utcfromtimestamp(b[n].stats.starttime.timestamp),
-        "endtime":datetime.utcfromtimestamp(b[n].stats.endtime.timestamp),
-        "sampling_rate":b[n].stats.sampling_rate,
-        "delta":b[n].stats.delta,
-        "npts":b[n].stats.npts,
-        "calib":b[n].stats.calib,
-        "data":b[n].data.tolist(), 
+            "network": b[n].stats.network,
+            "station": b[n].stats.station,
+            "location": b[n].stats.location,
+            "channel": b[n].stats.channel,
+            "starttime": datetime.utcfromtimestamp(b[n].stats.starttime.timestamp),
+            "endtime": datetime.utcfromtimestamp(b[n].stats.endtime.timestamp),
+            "sampling_rate": b[n].stats.sampling_rate,
+            "delta": b[n].stats.delta,
+            "npts": b[n].stats.npts,
+            "calib": b[n].stats.calib,
+            "data": b[n].data.tolist(),
+            "file_name": el
         }
-        insertion = DataDB.insert_one(post)
-        n=n+1
+        
+        DataDB.insert_one(post)
+        n = n+1
+        
+
 
 def AddedFiles(FileName):
-    post2 ={
-        "file_name":FileName,
-        "add_time":today
+    post2 = {
+        "file_name": FileName,
+        "add_time": today
     }
-    insertion = AddedFilesDB.insert_one(post2)
+    AddedFilesDB.insert_one(post2)
 
+
+def DatabaseChecker(datas):
+    for datas in Files:
+        DB_INSERT(datas)
+        AddedFiles(datas)
+
+
+    # print(list(F))
+n = 0
+
+
+'''
+for e in Files:
+    #AdFiles = AddedFilesDB.find_one({"file_name"},{e})
+    #FileNameInDB = DataDB.find_one({"file_name"},{e})
+
+    #AdFiles = AddedFilesDB.find_one({"file_name":e})
+    #FileNameInDB = DataDB.find_one({"file_name":e})
+
+    AdFiles = AddedFilesDB.find({},{"_id":0,"file_name":1})
+
+
+    #FileNameInDB = DataDB.find_one({"file_name":e})
+
+
+    print(AdFiles)
+    #print(FileNameInDB)
+   # print(FileNameInDB)
+   # DB_INSERT(e)
+   # AddedFiles(e)
+  '''
+'''
+for e in AddedFilesDB.find({}, {"_id": 0, "file_name": 1}):
+    #DatabaseChecker(e)
+    
+    print(type(e))
+    res = str(e)
+    lenres = len(res)
+    convertedstring = (res[13:lenres-1])
+    XX = re.sub("'", "", convertedstring)
+    XX2 = re.sub("^\s+|\s+$", "", XX, flags=re.UNICODE)
+    for e1 in DataDB.find({}, {"_id": 0, "file_name": 1}):
+        res2 = str(e1)
+        print(res2)
+
+        lenres2 = len(res2)
+        convertedstring2 = (res2[13:lenres2-1])
+        XXX = re.sub("'", "", convertedstring2)
+        XX3 = re.sub("^\s+|\s+$", "", XXX, flags=re.UNICODE)
+
+        if XX2 == XX3:
+                print("Equal")
+                #break
+        elif XX2 != XX3:
+                print("NOT Equals")
+                #print(str(datas))
+                print(XX2)
+                print(XX3)
+                #DatabaseChecker(e)
+
+        # e+=1
+'''
 
 for e in Files:
+   # AA=DataDB.find({"/home/zoohan/Рабочий стол/convert_mongo/MiniseedConvert/tst/IV.KRMSH_centaur-6_7618_20210401_210000.miniseed":{"$exists":True}})
+    #AA=list(AddedFilesDB.find({"$and":[ {e:{"$exists": True}}, {e:{"$ne": "1"}}]}))
+    AA=AddedFilesDB.find()[20:25]
+    BB=DataDB.find({"$and":[ {e:{"$exists": True}}, {e:{"$ne": ""}}]})
+    print(e)
+#collection.find({"cwc":{"$exists":True}})
+    for doc in AddedFilesDB.find(({e:{"$exists":True}})):
+        print(doc)
+            
+    
+    #print("False")
+    
     DB_INSERT(e)
     AddedFiles(e)
-
+  
 server.stop()
-
-
